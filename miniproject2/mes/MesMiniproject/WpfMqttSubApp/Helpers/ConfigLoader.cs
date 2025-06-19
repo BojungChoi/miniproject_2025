6ec89d692982b7593bce235fcf64922f5f541033
BojungChoi <fake5378@naver.com>
2025-06-19 11:49:20 +0900

    MQTT Sub 까지 완..

---
+++ miniproject2/mes/MesMiniproject/WpfMqttSubApp/Helpers/ConfigLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Text.Json;
+using WpfMqttSubApp.Models;
+
+namespace WpfMqttSubApp.Helpers
+{
+    public static class ConfigLoader
+    {
+        public static TotalConfig Load(string path= "config.json")
+        {
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException($"설정파일이 없습니다.", path);
+            }
+
+            string json = File.ReadAllText(path); // 문자열로 읽음
+            var config = JsonSerializer.Deserialize<TotalConfig>(json);
+            
+            if (config == null)
+            {
+                throw new InvalidDataException("설정파일이 올바르지 않습니다.");
+            }
+            return config;
+        }
+    }
+}
